038ab936dad54e1864be252e4413852d7646a290
yl <2201441955@qq.com>
2025-10-14 12:16:47 +0800

    1

--- publish.sh
+++ publish.sh
@@ -1,6 +1,3 @@
 dotnet publish ./CryptoExchangeWrapper -r linux-x64 -c Release
 
-# 
-# cp ./CryptoExchangeWrapper/bin/Release/net8.0/linux-x64/publish/*.* ~/f0cii/mojo-experiments/libs/
-# 
-cp ./CryptoExchangeWrapper/bin/Release/net8.0/linux-x64/publish/*.* ~/f0cii/trade-forge/libs/
+# cp ./CryptoExchangeWrapper/bin/Release/net8.0/linux-x64/publish/*.* ~/f0cii/pure-strat/libs/
